7e373445b68339340e8689aa3c1a9886f013497e
jmugisha1 <j.mugisha1@alustudent.com>
2024-02-25 12:59:52 +0200

    commit

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -4,32 +4,31 @@ and then export the result to csv file. """
 
 import json
 import requests
+import sys
 
 
 def main():
     """main function"""
+    user_id = int(sys.argv[1])
     todo_url = 'https://jsonplaceholder.typicode.com/todos'
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
 
     response = requests.get(todo_url)
-
-    output = {}
+    user_name = requests.get(user_url).json().get('username')
+    user_data = []
+    output = {user_id: user_data}
 
     for todo in response.json():
-        user_id = todo.get('userId')
-        if user_id not in output.keys():
-            output[user_id] = []
-            user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(
-                user_id)
-            user_name = requests.get(user_url).json().get('username')
-
-        output[user_id].append(
-            {
-                "username": user_name,
-                "task": todo.get('title'),
-                "completed": todo.get('completed')
-            })
-
-    with open("todo_all_employees.json", 'w') as file:
+        if todo.get('userId') == user_id:
+            user_data.append(
+                {
+                    "task": todo.get('title'),
+                    "completed": todo.get('completed'),
+                    "username": user_name,
+                })
+    print(output)
+    file_name = "{}.json".format(user_id)
+    with open(file_name, 'w') as file:
         json.dump(output, file)
 
 
